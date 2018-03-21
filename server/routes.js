80c5776a84b9cfa99e5a882e6ddcfb145833adcb
aloralyn <aloralyn@gmail.com>
2018-03-21 10:37:43 -0400

    - Require async/await - Include router in server/index.js

---
+++ server/routes.js
@@ -0,0 +1,60 @@
+const bmtRouter = require('express').Router();
+const companyController = require('../database/models/companies.js');
+const departmentController = require('../database/models/departments.js');
+const employeeController = require('../database/models/employees.js');
+
+const async = require('asyncawait/async');
+const await = require('asyncawait/await');
+
+bmtRouter.post('/api/create_company', async (req, res) => {
+  try {
+    await companyController.createCompany(req.body)
+    res.status(200).end();
+  } catch(e) {
+    res.status(400).end();
+  }
+});
+  
+bmtRouter.post('/api/update_company/:id', async (req, res) => {
+  let id = req.params.id;
+
+  try {
+    await companyController.updateCompany(id, req.body)
+    res.status(200).end();
+  } catch(e) {
+    res.status(400).end();
+  }
+});
+
+bmtRouter.post('/api/create_department/:id', async (req, res) => {
+  let id = req.params.id;
+
+  try {
+    await departmentController.createDepartment(id, req.body)
+    res.status(200).end();
+  } catch(e) {
+    res.status(400).end();
+  }
+});
+
+bmtRouter.post('/api/update_department/:id', async (req, res) => {
+  let id = req.params.id;
+
+  try {
+    await departmentController.updateDepartment(id, req.body)
+    res.status(200).end();
+  } catch(e) {
+    res.status(400).end();
+  }
+});
+
+bmtRouter.post('/api/create_employee_profile', async (req, res) => {
+  try {
+    await employeeController.createEmployeeProfile(req.body)
+    res.status(200).end();
+  } catch(e) {
+    res.status(400).end();
+  }
+});
+
+module.exports = bmtRouter;
