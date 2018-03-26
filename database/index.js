751d51b3f802c058f72e52a49efacb6a832789ed
chrisr <christopher.rigoli@gmail.com>
2018-03-26 19:31:35 -0400

    quick fix within db index.js for local vs live testing

--- database/index.js
+++ database/index.js
@@ -3,21 +3,25 @@ const pg = require('pg');
 const { Pool, Client } = require('pg');
 //Guys, be sure to create your database in your postgres cli first before running your schema.sql file
 
-const config = {
-  user: 'christopherrigoli',
-  host: 'localhost',
-  password: '',
-  database: 'bmttools',
-  port: 5432,
-};
-
+// @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @
+// @ DO NOT DELETE, CHRIS IS USING THIS CONFIG FOR LOCAL TESTING
+// @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @
 // const config = {
-//   user: "greyjoy",
-//   host: "bmttools.co5tjr5irgfy.us-east-2.rds.amazonaws.com",
-//   password: "chrisbrentartemlyn369",
-//   database: "bmt",
-//   port: 5432
-// }
+//   user: 'christopherrigoli',
+//   host: 'localhost',
+//   password: '',
+//   database: 'bmttools',
+//   port: 5432,
+// };
+// @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @
+
+const config = {
+  user: "greyjoy",
+  host: "bmttools.co5tjr5irgfy.us-east-2.rds.amazonaws.com",
+  password: "chrisbrentartemlyn369",
+  database: "bmt",
+  port: 5432
+}
 
 const client = new Client(config)
 
