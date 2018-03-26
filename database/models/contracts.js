fb6fc3b2d5f3fd70f125d2965e75e914fb52081e
chrisr <christopher.rigoli@gmail.com>
2018-03-26 19:23:46 -0400

    ENGR-0066_updated all schema tables, updated dummy data

--- database/models/contracts.js
+++ database/models/contracts.js
@@ -10,3 +10,14 @@ exports.addContract = (awardedTo, companyID, clientName, contractName, contractA
     }
   });
 };
+
+exports.addContract = (awardedTo, companyID, clientName, contractName, contractAmount, startDate, endDate, cb) => {
+  const queryString = 'INSERT INTO contracts (awaredTo_ID, company_id, clientName, contractName, contractAmount, contractStartDate, contractEndDate) VALUES (?, ?, ?, ?, ?, ?, ?)';
+  db.query(queryString, [awardedTo, companyID, clientName, contractName, contractAmount, startDate, endDate], (err, result) => {
+    if (err) {
+      console.log('ERROR in db function addContract, error: ', err);
+    } else {
+      cb(result);
+    }
+  });
+};
