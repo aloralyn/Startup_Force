c574c1c3f066ab58157e5497a426aecac94860bc
chrisr <christopher.rigoli@gmail.com>
2018-04-05 15:12:20 -0400

    add additional styling to contracts and reports page

--- client/src/components/Reports/Reports.jsx
+++ client/src/components/Reports/Reports.jsx
@@ -12,53 +12,6 @@ class Reports extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentChart: '',
-      employeeData: null,
-      chartData: null,
-      currentChartDisplayed: '',
-      reportOptions: [
-        {
-          key: 'Sales Figures',
-          value: 'Sales Figures',
-          text: 'Sales Figures',
-        },
-        {
-          key: 'Employee Stats',
-          value: 'Employee Stats',
-          text: 'Employee Stats',
-        },
-        {
-          key: 'Hours worked',
-          value: 'Hours worked',
-          text: 'Hours worked',
-        },
-      ],
-    };
-    this.handleDropdownClick.bind(this);
-  }
-
-  handleDropdownClick = (e, data) => {
-    const { value } = data;
-    axios.post('/get/employee/data', { data: 'data' })
-      .then((res) => {
-        console.log('successful post with res: ', res.data.rows);
-        this.setState({ chartData: res.data.rows });
-      })
-      .catch((err) => {
-        console.log('ERROR in handleDropdownClick axios post, error: ', err);
-      });
-
-    if (this.state.currentChart !== value) {
-      this.state.reportOptions.forEach((option) => {
-        if (value === option.value && this.props.currentChart !== option.value) {
-          console.log(`Render ${option.text} Chart`);
-          this.setState({
-            currentChart: option.value,
-          });
-        }
-      });
-    } else {
-      console.log('same chart, do not rerender');
     }
   }
 
@@ -67,20 +20,9 @@ class Reports extends React.Component {
       <div className="App-chart-container">
         <Grid>
           <Grid.Row />
-          <Grid.Row columns={2}>
-            <Grid.Column width={3}>
-              <Dropdown
-                fluid
-                selection
-                placeholder="Report Options"
-                options={this.state.reportOptions}
-                onChange={this.handleDropdownClick}
-              />
-            </Grid.Column>
-            <Grid.Column width={7}>
-              <Metrics />
-            </Grid.Column>
-          </Grid.Row>
+          <Grid.Column width={9}>
+            <Metrics />
+          </Grid.Column>
         </Grid>
       </div>
     );
